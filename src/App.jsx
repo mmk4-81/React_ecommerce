77cd4749d5b4319c6f195bdd76e452d48f46af08
mmk <mmkkarbalaei@gmail.com>
2024-05-24 19:22:07 +0330

    create main pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
+import Shops from './pages/Shops/Shops';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+import Profile from './pages/Profile/Profile';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
   return (
@@ -8,7 +15,7 @@ function App() {
           <ul>
             <li><Link to="/">خانه</Link></li>
             <li><Link to="/products">محصولات</Link></li>
-            <li><Link to="/stores">فروشگاه ها</Link></li>
+            <li><Link to="/shops">فروشگاه ها</Link></li>
             <li><Link to="/login">ورود</Link></li>
             <li><Link to="/signup">ثبت نام</Link></li>
             <li><Link to="/profile">پروفایل</Link></li>
@@ -17,15 +24,15 @@ function App() {
         </nav>
 
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-          {/* <Route path="/products" element={<Products />} />
-          <Route path="/stores" element={<Stores />} />
-          <Route path="/products/:productId" element={<ProductDetails />} />
-          <Route path="/stores/:storeId" element={<StoreDetails />} />
+          <Route path="/" element={<Home />} />
+           <Route path="/products" element={<Products />} />
+          <Route path="/shops" element={<Shops />} />
+          {/* <Route path="/products/:productId" element={<ProductDetails />} /> */}
+          {/* <Route path="/shops/:shopId" element={<shopDetails />} /> */}
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/dashboard" element={<SellerDashboard />} /> */}
+          <Route path="/dashboard" element={<Dashboard />} /> 
         </Routes>
       </div>
     </BrowserRouter>
