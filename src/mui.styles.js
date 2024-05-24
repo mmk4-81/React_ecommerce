027d77a29a423c1e7da84e9212b836340bab7592
mmk <mmkkarbalaei@gmail.com>
2024-05-24 18:56:39 +0330

    create mui style

---
+++ src/mui.styles.js
@@ -0,0 +1,9 @@
+import { styled } from '@emotion/styled';
+import { Button } from '@mui/material';
+
+export const MyButton = styled(Button)`
+  background-color: #f00;
+  color: #fff;
+  padding: 10px 20px;
+`;
+
