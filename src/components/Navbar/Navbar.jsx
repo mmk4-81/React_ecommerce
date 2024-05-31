fb46aaa38cae061b8729edb519061702511fd7e6
mmk <mmkkarbalaei@gmail.com>
2024-05-31 20:06:44 +0330

    navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,167 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import HomeIcon from '@mui/icons-material/Home';
+import StoreIcon from '@mui/icons-material/Store';
+import CategoryIcon from '@mui/icons-material/Category';
+import logo from './../../assets/logo/logo.png';
+
+const pages = [
+  { id: 'home', name: 'صفحه اصلی', link: '/', icon: <HomeIcon /> },
+  { id: 'products', name: 'محصولات', link: '/products', icon: <CategoryIcon /> },
+  { id: 'shops', name: 'فروشگاه‌ها', link: '/shops', icon: <StoreIcon /> },
+];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const isLoggedIn = true; // شرط لاگین بودن کاربر
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="fixed" elevation={0} sx={{ backgroundColor: 'white', height: 80 }}>
+      <Container maxWidth="xl">
+        <Toolbar sx={{ height: 80, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          {/* Logo for mobile */}
+          <Box sx={{ display: { xs: 'flex', md: 'none' }, justifyContent: 'center', flexGrow: 1 }}>
+            <Link to="/">
+              <img src={logo} alt="Logo" style={{ height: 70, width: 70 }} />
+            </Link>
+          </Box>
+
+          {/* Hamburger menu for mobile */}
+          <Box sx={{ display: { xs: 'flex', md: 'none' }, alignItems: 'center', position: 'absolute', right: 0 }}>
+            <IconButton
+              size="large"
+              aria-label="menu"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              sx={{ color: '#8d494a' }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              PaperProps={{
+                sx: {
+                  width: '50%',
+                  height: '30vh',
+                  top: '10px',
+                  left: '0',
+                  right: '0',
+                  bottom: '0',
+                  display: 'flex',
+                  justifyContent: 'right',
+                  alignItems: 'start',
+                  borderRadius:'10px'
+                },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page.id}
+                  component={Link}
+                  to={page.link}
+                  onClick={handleCloseNavMenu}
+                  sx={{ color: '#8d494a', justifyContent: 'start', width: '100%' }}
+                >
+                  {page.icon}
+                  <Typography sx={{ mr: 2 }}>{page.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          {/* Logo and menu for desktop */}
+          <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center', justifyItems: 'right' }}>
+            <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}>
+              <Link to="/">
+                <img src={logo} alt="Logo" style={{ height: 70, width: 80, marginTop: 10 }} />
+              </Link>
+            </Box>
+
+            {/* Pages for desktop */}
+            <Box sx={{ display: { xs: 'none', md: 'flex' }, mr: 10, gap: 2 }}>
+              {pages.map((page) => (
+                <Link
+                  key={page.id}
+                  to={page.link}
+                  style={{ margin: '0 8px', color: '#8d494a', textDecoration: 'none', padding: '16px 0' }}
+                >
+                  {page.name}
+                </Link>
+              ))}
+            </Box>
+          </Box>
+
+          {/* Right side icons and buttons */}
+          <Box sx={{ display: 'flex', alignItems: 'center', marginRight: '20px' }}>
+            <IconButton color="inherit" component={Link} to="/search">
+              <SearchIcon sx={{ color: '#8d494a' }} />
+            </IconButton>
+            <IconButton color="inherit" component={Link} to="/cart">
+              <ShoppingCartIcon sx={{ color: '#8d494a' }} />
+            </IconButton>
+            {!isLoggedIn ? (
+              <Button component={Link} to="/login" sx={{ color: 'white', backgroundColor: '#8d494a', borderRadius: '20px', padding: '5px 15px', ml: 1 }}>
+                ورود
+              </Button>
+            ) : (
+              <>
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <AccountCircle  sx={{ color: '#8d494a', fontSize: 30 }} />
+                </IconButton>
+                <Button component={Link} to="/logout" sx={{ color: '#8d494a', ml: 1 }}>
+                  خروج
+                </Button>
+              </>
+            )}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default ResponsiveAppBar;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,58 @@
-// import React from 'react'
+import { Container, Typography, TextField, Button, Box } from '@mui/material';
+import { useState } from 'react';
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+ 
   return (
-    <div>Login</div>
-  )
+    <Container maxWidth="sm">
+      <Box mt={8}>
+        <Typography variant="h4" align="center" gutterBottom>
+          ورود به حساب کاربری
+        </Typography>
+        <form >
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="نام کاربری"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="رمز عبور"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            size="large"
+            sx={{ mt: 2 }}
+          >
+            ورود
+          </Button>
+        </form>
+      </Box>
+    </Container>
+  );
 }
 
 export default Login
