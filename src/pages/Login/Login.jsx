290982436518bd22e99b11b0114230e0616dac83
mmk <mmkkarbalaei@gmail.com>
2024-06-05 22:43:54 +0330

    login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,58 +1,158 @@
-import { Container, Typography, TextField, Button, Box } from '@mui/material';
+import { Container, Typography, TextField, Button, Box, Link } from '@mui/material';
 import { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+import pic from './../../assets/img/back.jpg';
 
 const Login = () => {
+  const theme = useTheme();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
- 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <Container maxWidth="sm">
-      <Box mt={8}>
-        <Typography variant="h4" align="center" gutterBottom>
-          ورود به حساب کاربری
-        </Typography>
-        <form >
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="نام کاربری"
-            name="username"
-            autoComplete="username"
-            autoFocus
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="رمز عبور"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            size="large"
-            sx={{ mt: 2 }}
-          >
-            ورود
-          </Button>
-        </form>
-      </Box>
-    </Container>
+    <Box
+      sx={{
+        minHeight: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundImage: `url(${pic})`,
+        backgroundSize: 'cover',
+      }}
+    >
+      <Container maxWidth="sm">
+        <Box 
+          sx={{ 
+            backgroundPosition: 'center',
+            backgroundColor: 'rgba(255, 255, 255, 0.8)', 
+            padding: '40px', 
+            borderRadius: '8px',
+            textAlign: 'right', 
+            direction: 'rtl', 
+            fontFamily: 'Vazir', 
+          }}
+        >
+          <Typography variant="h6" align="center" gutterBottom>
+            ورود به حساب کاربری
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="نام کاربری"
+              name="username"
+              autoComplete="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              InputLabelProps={{
+                sx: {
+                  fontSize: '0.800rem', // Smaller font size
+                  right: '40px', // Align to the right
+                  transformOrigin: 'top right', // Adjust transform origin
+                  '&.Mui-focused': {
+                    color: 'black', // Change color when focused
+                  },
+                  '&.MuiInputLabel-shrink': {
+                    transformOrigin: 'top right', // Align when focused
+                  },
+                },
+              }}
+              InputProps={{
+                sx: {
+                  '& input:focus': { // Change focus color
+                    backgroundColor: '#ffffff',
+                  },
+                },
+              }}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="رمز عبور"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              InputLabelProps={{
+                sx: {
+                  fontSize: '0.800rem', // Smaller font size
+                  right:'40px', // Align to the right
+                  transformOrigin: 'top right', // Adjust transform origin
+                  '&.Mui-focused': {
+                    color: 'black', // Change color when focused
+                  },
+                  '&.MuiInputLabel-shrink': {
+                    transformOrigin: 'top right', // Align when focused
+                  },
+                },
+              }}
+              InputProps={{
+                sx: {
+                  '& input:focus': { // Change focus color
+                    backgroundColor: '#ffffff',
+                  },
+                },
+              }}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ 
+                
+                mt: 2, 
+                backgroundColor: theme.palette.secondary.main, 
+                color: "#fff", 
+                fontFamily: 'Vazir',
+                '&:hover': {
+                  backgroundColor: '#a65b5c', 
+                },
+              }}
+            >
+              ورود
+            </Button>
+          </form>
+          <Box sx={{ mt: 2, textAlign: 'center' }}>
+            <Typography variant="body2" gutterBottom>
+              اگر ثبت نام نکرده اید:
+            </Typography>
+            <Button
+              component={RouterLink}
+              to="/signup"
+              fullWidth
+              variant="contained"
+              sx={{ 
+                width:'50%',
+                color: "#fff", 
+                mt: 1, 
+                backgroundColor: theme.palette.secondary.main, 
+                fontFamily: 'Vazir',
+                '&:hover': {
+                  backgroundColor: '#a65b5c', 
+                },
+              }}
+            >
+              ثبت نام
+            </Button>
+            <Link component={RouterLink} to="/" variant="body2" sx={{ display: 'block', mt: 2, color: theme.palette.secondary.main, fontFamily: 'Vazir' }}>
+              بازگشت به صفحه اصلی سایت
+            </Link>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
-export default Login
+export default Login;
